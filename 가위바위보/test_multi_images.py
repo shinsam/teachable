e70c18d1f350a430d091ce56f9c6c8c6deb877fc
shinsam <shin321gg@gmail.com>
2026-04-13 00:08:13 +0900

    glob

--- 가위바위보/test_multi_images.py
+++ 가위바위보/test_multi_images.py
@@ -1,5 +1,6 @@
 from keras.models import load_model
 from PIL import Image, ImageOps
+import glob # 폴더 내 파일 검색을 위한 라이브러리
 import numpy as np
 import os  # 폴더 내 파일 목록을 가져오기 위해 필요
 import cv2 # 이미지를 화면에 보여주고 키보드 입력을 받기 위해 필요
@@ -15,15 +16,13 @@ model = load_model("keras_Model.h5", compile=False)
 class_names = open("labels.txt", "r", encoding="utf-8").readlines()
 
 # 3. 입력 데이터 저장소(4차원 배열) 미리 준비
-# 루프 밖에서 한 번만 만들어 효율성을 높입니다.
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 # 4. 이미지 폴더 탐색
 # 현재 폴더(.) 내의 파일 중 이미지 확장자만 골라냅니다.
 valid_extensions = ('.jpg', '.jpeg', '.png', '.bmp') #튜플형태로 확장자 정의할 경우
-#valid_extensions ='.jpg' 
-image_files = [f for f in os.listdir('imgs\\') if f.lower().endswith(valid_extensions)]
-# imgs 폴더 내의 파일 중에서 .jpg 확장자로 끝나는 파일만 리스트에 저장
+image_files = [f for f in glob.glob('imgs\\*') if f.lower().endswith(valid_extensions)]
+# imgs 폴더 내의 파일 중에서 지정된 확장자로 끝나는 파일만 리스트에 저장
 
 
 if not image_files:
@@ -36,7 +35,7 @@ print("▶ 아무 키나 누르면 다음 이미지로 넘어갑니다. (종료:
 # 5. 폴더 내 모든 이미지 반복 처리
 for file_name in image_files:
     # 이미지 불러오기
-    image = Image.open(f'imgs\\{file_name}').convert("RGB")
+    image = Image.open(f'{file_name}').convert("RGB")
     
     # 전처리: Center Crop (224x224)
     size = (224, 224)
@@ -57,11 +56,14 @@ for file_name in image_files:
     # PIL 이미지를 OpenCV에서 보여줄 수 있도록 변환 (RGB -> BGR)
     display_img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
     
-    # 이미지 위에 텍스트 쓰기 (결과와 확신도)
+    # 이미지 위에 텍스트 쓰기 (결과와 신뢰도 표시)
     result_text = f"Result: {class_name[2:]} ({confidence_score:.2%})"
     cv2.putText(display_img, result_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
     cv2.putText(display_img, "Quit:q", (10, 210), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
+    #디버깅 정보
+    print(f"파일명: {file_name} 예측 결과: {class_name[2:]} 신뢰도: {confidence_score:.2%})") #파일명과 예측 결과를 줄바꿈으로 구분하여 출력
+    
     # 화면에 표시
     cv2.imshow("AI Prediction Test", display_img)
 
